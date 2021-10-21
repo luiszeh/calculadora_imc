fc2e1c75987142c36b017b5410b333e903d26f64
Luis Fernando Fonseca <luisffg91@gmail.com>
2021-10-21 00:54:32 -0300

    colocando exemplos de casas decimais e indicando kg e m

--- src/App.js
+++ src/App.js
@@ -1,24 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+const tabelImc = () => {
+  return (
+    <table border="1" style={{ borderCollapse: "collapse" }}>
+      <thead>
+        <tr>
+          <th>Classificação</th>
+          <th>IMC</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Abaixo do Peso</td>
+          <td>Abaixo de 18.5</td>
+        </tr>
+        <tr>
+          <td>Peso Normal</td>
+          <td>Entre 18.5 e 24.9</td>
+        </tr>
+        <tr>
+          <td>Sobrepeso</td>
+          <td>Entre 25 e 29.9</td>
+        </tr>
+        <tr>
+          <td>Obesidade grau I</td>
+          <td>Entre 30 e 34.9</td>
+        </tr>
+        <tr>
+          <td>Obesidade grau II</td>
+          <td>AEntre 35 e 39.9</td>
+        </tr>
+        <tr>
+          <td>Obesidade grau III</td>
+          <td>Acima de 40</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+const fPeso = (p, sp) => {
+  return (
+    <div>
+      <label>
+        Peso (kg)
+        <input
+          placeholder="Ex: 60 / 65.9"
+          type="text"
+          value={p}
+          onChange={(e) => {
+            sp(e.target.value);
+          }}
+        />
+      </label>
+      <p>Ex: 60 / 65.9</p>
+    </div>
+  );
+};
+
+const fAltura = (a, sa) => {
+  return (
+    <div>
+      <label>
+        Altura (m)
+        <input
+          placeholder="Ex: 1 / 1.75 / 2 "
+          type="text"
+          value={a}
+          onChange={(e) => {
+            sa(e.target.value);
+          }}
+        />
+      </label>
+      <p>Ex: 1 / 1.75 / 2</p>
+    </div>
+  );
+};
+
+const fCalcular = (p, a, sr) => {
+  const calc = () => {
+    sr(p / (a * a));
+  };
+
+  return (
+    <div>
+      <button onClick={calc}>Calcular</button>
+    </div>
+  );
+};
+
+const fResultado = (r) => {
+  return (
+    <div>
+      <p>Resultado: {r.toFixed(2)}</p>
+    </div>
+  );
+};
 
 function App() {
+  const [peso, setPeso] = useState(0);
+  const [altura, setAltura] = useState(0);
+  const [resultado, setResultado] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {fPeso(peso, setPeso)}
+      {fAltura(altura, setAltura)}
+      {fCalcular(peso, altura, setResultado)}
+      {fResultado(resultado)}
+      {tabelImc()}
+    </>
   );
 }
 
